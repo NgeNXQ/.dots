b309df507e0930dc2fa46c38398118e6d4685726
Denys Babich <89969565+NgeNXQ@users.noreply.github.com>
2025-03-05 20:59:20 +0200

    Change title to breadcrumbs, add shortcuts, QOL

--- vscode/settings.js
+++ vscode/settings.js
@@ -1,12 +1,11 @@
 let windowTitle;
-let paneCompositePart;
 let explorerTitleLabel;
 
 let suggestWidget;
 let monacoListRows;
 let suggestDetailsContainer;
 
-function getElementWithIntervalBySelector(selector, pollingInterval = 10) {
+function getElementWithInterval(selector, pollingInterval = 10) {
     return new Promise((resolve) => {
         const intervalId = setInterval(() => {
             const cachedElement = document.querySelector(selector);
@@ -19,10 +18,28 @@ function getElementWithIntervalBySelector(selector, pollingInterval = 10) {
     });
 }
 
+function changeSuggest() {
+    const maxChildCount = 13;
+
+    getElementWithInterval('.suggest-widget').then(element => {
+        suggestWidget = element;
+    });
+
+    getElementWithInterval('.suggest-widget .tree .monaco-list .monaco-scrollable-element .monaco-list-rows').then(element => {
+        monacoListRows = element;
+
+        const observer = new MutationObserver(() => {
+            const height = monacoListRows.childElementCount < maxChildCount ? monacoListRows.style.height : suggestWidget.style.height;
+            document.documentElement.style.setProperty('--custom-suggest-height', `${height}`);
+        });
+
+        observer.observe(monacoListRows, { attributes: true });
+    });
+}
+
 function changeExplorerTitle() {
-    getElementWithIntervalBySelector('.pane-composite-part:has(.content .explorer-viewlet)').then(element => {
-        paneCompositePart = element;
-        explorerTitleLabel = paneCompositePart.querySelector('.composite.title .title-label h2');
+    getElementWithInterval('.pane-composite-part:has(.content .explorer-viewlet) .composite.title .title-label h2').then(element => {
+        explorerTitleLabel = element;
 
         const observer = new MutationObserver(() => {
             if (explorerTitleLabel && windowTitle) {
@@ -30,10 +47,10 @@ function changeExplorerTitle() {
             }
         });
 
-        observer.observe(explorerTitleLabel, { childList: true, subtree: true });
+        observer.observe(explorerTitleLabel, { childList: true });
     });
 
-    getElementWithIntervalBySelector('.window-title').then(element => {
+    getElementWithInterval('.window-title').then(element => {
         windowTitle = element;
 
         const observer = new MutationObserver(() => {
@@ -42,43 +59,25 @@ function changeExplorerTitle() {
             }
         });
 
-        observer.observe(windowTitle, { childList: true, subtree: true });
+        observer.observe(windowTitle, { childList: true });
     });
 }
 
 function changeSuggestDetails() {
-    getElementWithIntervalBySelector('.suggest-details-container').then(element => {
+    getElementWithInterval('.suggest-details-container').then(element => {
         suggestDetailsContainer = element;
     });
 
-    getElementWithIntervalBySelector('.suggest-widget').then(element => {
+    getElementWithInterval('.suggest-widget').then(element => {
         suggestWidget = element;
 
         const observer = new MutationObserver(() => {
             const widgetRect = suggestWidget.getBoundingClientRect();
-            suggestDetailsContainer.style.setProperty('--custom-suggest-details-left', `${widgetRect.left}px`);
-            suggestDetailsContainer.style.setProperty('--custom-suggest-details-top', `${widgetRect.bottom + 1}px`);
+            document.documentElement.style.setProperty('--custom-suggest-details-left', `${widgetRect.left}px`);
+            document.documentElement.style.setProperty('--custom-suggest-details-top', `${widgetRect.bottom + 1}px`);
         });
 
-        observer.observe(suggestWidget, { childList: true, subtree: true });
-    });
-}
-
-function changeSuggest() {
-    const maxChildCount = 13;
-
-    getElementWithIntervalBySelector('.suggest-widget').then(element => {
-        suggestWidget = element;
-    });
-
-    getElementWithIntervalBySelector('.suggest-widget .tree .monaco-list .monaco-scrollable-element .monaco-list-rows').then(element => {
-        monacoListRows = element;
-
-        const observer = new MutationObserver(() => {
-            suggestWidget.style.height = monacoListRows.childElementCount < maxChildCount ? monacoListRows.style.height : '';
-        });
-
-        observer.observe(suggestWidget, { attributes: true, childList: true, subtree: true });
+        observer.observe(suggestWidget, { attributes: true });
     });
 }
 
